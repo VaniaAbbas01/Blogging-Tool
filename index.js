e796dc2f00f7ee00711091ef3ad8184220c268f9
Vania Abbas <vaniaabbas2@gmail.com>
2025-08-23 23:30:41 +0500

    added authorize route code

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(express.static(__dirname + '/public')); // set location of static files
 // Set up SQLite
 // Items in the global namespace are accessible throught out the node application
 const sqlite3 = require('sqlite3').verbose();
-global.db = new sqlite3.Database('./database.db',function(err){
-    if(err){
+global.db = new sqlite3.Database('./database.db', function (err) {
+    if (err) {
         console.error(err);
         process.exit(1); // bail out we can't connect to the DB
     } else {
@@ -27,49 +27,50 @@ global.db = new sqlite3.Database('./database.db',function(err){
 
 // Handle requests to the home page 
 app.get('/', (req, res) => {
-    const title = {
-        name: "Pen Your Thoughts",
-        sub_message: "If You Can Think It, Pen It"
-    }
-    res.render('index', {title});
+    res.render('index');
 });
 
 /* START OF SELF-WRITTEN CODE */
 
-    // Add all the route handlers in readerRoutes to the app under the path /reader
-    const readerRoutes = require('./routes/reader');
-    // anything that begins with "reader" go to the reader file
-    app.use('/reader', readerRoutes);
+// Add all the route handlers in authorize to the app under the path /authorize
+const authorizeRoutes = require('./routes/authorize');
+// anything that begins with "authorize" go to the authorize file
+app.use('/authorize', authorizeRoutes);
 
-    // Add all the route handlers in authorRoutes to the app under the path /author
-    const auhtorRoutes = require('./routes/author');
-    // anything that begins with "authors" go to the author file
-    app.use('/author', auhtorRoutes);
+// Add all the route handlers in readerRoutes to the app under the path /reader
+const readerRoutes = require('./routes/reader');
+// anything that begins with "reader" go to the reader file
+app.use('/reader', readerRoutes);
 
-    // Add all the route handlers in blogRoutes to the app under the path /blogs
-    const blogRoutes = require('./routes/blogs');
-    // anything that begins with "blogs" go to the blogs file
-    app.use('/blogs', blogRoutes);
+// Add all the route handlers in authorRoutes to the app under the path /author
+const auhtorRoutes = require('./routes/author');
+// anything that begins with "authors" go to the author file
+app.use('/author', auhtorRoutes);
 
-    // Add all the route handlers in comment to the app under the path /comments
-    const commentRoutes = require('./routes/comments');
-    // anything that begins with "comments" go to the comments file
-    app.use('/comments', commentRoutes);
+// Add all the route handlers in blogRoutes to the app under the path /blogs
+const blogRoutes = require('./routes/blogs');
+// anything that begins with "blogs" go to the blogs file
+app.use('/blogs', blogRoutes);
 
-    // Add all the route handlers in likes to the app under the path /likes
-    const likesRoutes = require('./routes/likes');
-    // anything that begins with "likes" go to the likes file
-    app.use('/likes', likesRoutes);
+// Add all the route handlers in comment to the app under the path /comments
+const commentRoutes = require('./routes/comments');
+// anything that begins with "comments" go to the comments file
+app.use('/comments', commentRoutes);
 
-    // Add all the route handlers in dislikes to the app under the path /dislikes
-    const dislikesRoutes = require('./routes/dislikes');
-    // anything that begins with "dislikes" go to the dislikes file
-    app.use('/dislikes', dislikesRoutes);
+// Add all the route handlers in likes to the app under the path /likes
+const likesRoutes = require('./routes/likes');
+// anything that begins with "likes" go to the likes file
+app.use('/likes', likesRoutes);
 
-    // Add all the route handlers in reactions to the app under the path /reactions
-    const reactionsRoutes = require('./routes/reactions');
-    // anything that begins with "reactions" go to the dislikes file
-    app.use('/reactions', reactionsRoutes);
+// Add all the route handlers in dislikes to the app under the path /dislikes
+const dislikesRoutes = require('./routes/dislikes');
+// anything that begins with "dislikes" go to the dislikes file
+app.use('/dislikes', dislikesRoutes);
+
+// Add all the route handlers in reactions to the app under the path /reactions
+const reactionsRoutes = require('./routes/reactions');
+// anything that begins with "reactions" go to the dislikes file
+app.use('/reactions', reactionsRoutes);
 
 /* END OF SELF-WRITTEN CODE */
 
