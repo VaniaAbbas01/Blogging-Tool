ccaa1fa9a6566f88835b660641577390a91fa3d2
Vania Abbas <vaniaabbas2@gmail.com>
2025-08-23 23:29:59 +0500

    added token authorisation functionality to the routes

--- routes/author.js
+++ routes/author.js
@@ -1,62 +1,56 @@
 /* START OF CODE SELF-WRITTEN */
 
-    const express = require("express");
-    const router = express.Router();
+const express = require("express");
+const router = express.Router();
+const authorizeUser = require("./authorizeUser");
 
-    /* 
-        render Author Home Page
-        INPUT: user chooses author route in the home page
-        OUTPUT: Author home page rendered
-   */
-    router.get("/blogs", (req, res, next) => {
-        // Define the query
-        query = "SELECT blogs.id, blogs.title, blogs.content, blogs.created_at, blogs.published_at, blogs.last_modified, blogs.status, (SELECT COUNT(*) FROM likes WHERE blog_id = blogs.id) AS like_count, (SELECT COUNT(*) FROM dislikes WHERE blog_id = blogs.id) AS dislike_count, (SELECT COUNT(*) FROM comments WHERE blog_id = blogs.id) AS comment_count, (SELECT COUNT(*) FROM views WHERE blog_id = blogs.id) AS views_count FROM blogs";
-        // Execute the query and render the page with the results
-        global.db.all(query, 
-            function (err, rows) 
-            {
-                if (err) 
-                {
-                    next(err); //send the error on to the error handler
-                } 
-                else 
-                {
-                    res.render("author-home", {title: "Author's Page",
-                                                name: "Pen Your Thoughts",
-                                                nav1: "Home",
-                                                nav2: "Settings",
-                                                heading: "Pen Your Thoughts",
-                                                author: "Vania Abbas",
-                                                sub_heading: "Author's Home Page",
-                                                blogs: rows,
-                                                button1: "Create New Draft",
-                                                button2: "Edit",
-                                                button3: "Delete",
-                                                button4: "Publish",
-                                                button5: "Share"}); // render blogs page
-                }
+/* 
+    render Author Home Page
+    INPUT: user chooses author route in the home page
+    OUTPUT: Author home page rendered
+*/
+router.get("/blogs", authorizeUser, (req, res, next) => {
+    const id = req.id
+    // Define the query
+    query = `SELECT blogs.id, blogs.title,
+     blogs.content, blogs.created_at, blogs.published_at, blogs.last_modified, blogs.status, 
+     (SELECT COUNT(*) FROM likes WHERE blog_id = blogs.id) AS like_count, 
+     (SELECT COUNT(*) FROM dislikes WHERE blog_id = blogs.id) AS dislike_count, 
+     (SELECT COUNT(*) FROM comments WHERE blog_id = blogs.id) AS comment_count, 
+     (SELECT COUNT(*) FROM views WHERE blog_id = blogs.id) AS views_count 
+     FROM blogs WHERE blogs.user_id = ? ORDER BY blogs.created_at DESC;`
+    // Execute the query and render the page with the results
+    global.db.all(query, [id],
+        function (err, rows) {
+            if (err) {
+                next(err); //send the error on to the error handler
             }
-        );
+            else {
+                res.render("author-home", { blogs: rows }); // render blogs page
+            }
+        }
+    );
+});
+
+/* 
+    render Author Settings Page
+    INPUT: user clicks on settings page in Author Home Page
+    OUTPUT: Author Settings page rendered
+*/
+router.get("/settings", (req, res, next) => {
+    res.render("author-settings", {
+        title: "Author's Page",
+        name: "Pen Your Thoughts",
+        heading: "Author's Settings Page",
+        slot1: "Author Name",
+        slot2: "Blog Title",
+        button1: "Submit Settings",
+        button2: "Back To Author Home Page"
     });
-
-    /* 
-        render Author Settings Page
-        INPUT: user clicks on settings page in Author Home Page
-        OUTPUT: Author Settings page rendered
-   */
-    router.get("/settings", (req, res, next) => {
-        res.render("author-settings", {title: "Author's Page",
-                                        name: "Pen Your Thoughts",
-                                        heading: "Author's Settings Page",
-                                        slot1: "Author Name",
-                                        slot2: "Blog Title",
-                                        button1: "Submit Settings",
-                                        button2: "Back To Author Home Page"
-                                    });
-    });
+});
 
 
-    // Export the router object so index.js can access it
-    module.exports = router;
+// Export the router object so index.js can access it
+module.exports = router;
 
 /* END OF CODE SELF-WRITTEN */
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,186 +1,171 @@
 /* START OF CODE SELF-WRITTEN */
 
-    const express = require("express");
-    const router = express.Router();
-    const moment = require('moment');
+const express = require("express");
+const router = express.Router();
+const moment = require('moment');
+const authorizeUser = require("./authorizeUser");
 
-    /* 
-        render new-article Page
-        INPUT: user clicks on "Create a New Article" in Author Home Page
-        OUTPUT:  new-article page rendered
-   */
-    router.get('/new', (req, res) => {
-        res.render('new-article', { title: 'Create New Blog' ,
-                                    name: "Pen Your Thoughts",
-                                    heading: "Create Your Story Here",
-                                    slot1: "Name Your Story",
-                                    slot2: "Content",
-                                    slot3: "Author Name",
-                                    button1: "Create",
-                                    button2: "Back to Blogs"});
+/* 
+    render new-article Page
+    INPUT: user clicks on "Create a New Article" in Author Home Page
+    OUTPUT:  new-article page rendered
+*/
+router.get('/new', authorizeUser, (req, res) => {
+    res.render('new-article', {
+        title: 'Create New Blog',
+        name: "Pen Your Thoughts",
+        heading: "Create Your Story Here",
+        slot1: "Name Your Story",
+        slot2: "Content",
+        slot3: "Author Name",
+        button1: "Create",
+        button2: "Back to Blogs"
     });
+});
 
-    /* 
-        Create a new article (submit)
-        INPUT: user fills the form and creates an article
-        OUTPUT: data about the new blog is inserted in database
-   */
-    router.post('/new', (req, res) => {
-        const { title, content} = req.body;
-        const created_at = moment().format('YYYY-MM-DD HH:mm:ss');
-        const modified_at = moment().format('YYYY-MM-DD HH:mm:ss');
-        const status = "rough";
-        if(!title || !content)
-        {
-            return res.status(400).send("All Fields Are Required");
+/* 
+    Create a new article (submit)
+    INPUT: user fills the form and creates an article
+    OUTPUT: data about the new blog is inserted in database
+*/
+router.post('/new', (req, res) => {
+    const { title, content } = req.body;
+    const created_at = moment().format('YYYY-MM-DD HH:mm:ss');
+    const modified_at = moment().format('YYYY-MM-DD HH:mm:ss');
+    const status = "rough";
+    if (!title || !content) {
+        return res.status(400).send("All Fields Are Required");
+    }
+    db.run("INSERT INTO blogs (title, content, author_name, status, created_at, published_at, last_modified) VALUES (?, ?, 'Vania Abbas', ?, ?, 'Not Published', ?)", [title, content, status, created_at, modified_at], function (err) {
+        if (err) {
+            return console.log(err.message);
         }
-        db.run("INSERT INTO blogs (title, content, author_name, status, created_at, published_at, last_modified) VALUES (?, ?, 'Vania Abbas', ?, ?, 'Not Published', ?)", [ title, content, status, created_at, modified_at], function(err) {
-            if (err) 
-            {
-                return console.log(err.message);
-            }
-            console.log("Successfully added")
-            res.redirect('/author/blogs');
+        console.log("Successfully added")
+        res.redirect('/author/blogs');
+    });
+});
+
+/* 
+    Publish a new article (submit)
+    INPUT: user clicks on the publish button in Author's Home Page
+    OUTPUT: status is updated in the database from "rough" to "published"
+*/
+router.post('/:id', authorizeUser, (req, res) => {
+    const id = req.params.id;
+    const status = "published";
+    const published_at = moment().format('YYYY-MM-DD HH:mm:ss');
+    const query = 'UPDATE blogs SET status = ?, published_at = ? WHERE id = ?';
+
+    db.run(query, [status, published_at, id], function (err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        res.redirect('/author/blogs');
+    });
+});
+
+/* 
+    Render edit-article page 
+    INPUT: user clicks on the publish button in Author's Home Page
+    OUTPUT: status is updated in the database from "rough" to "published"
+*/
+router.get('/edit/:id', authorizeUser, (req, res) => {
+    const id = req.params.id;
+    db.get("SELECT * FROM blogs WHERE id = ?", [id], (err, row) => {
+        if (err) {
+            return console.log(err.message);
+        }
+        res.render('edit-article', {
+            title: 'Edit Article',
+            name: "Pen Your Thoughts",
+            nav1: "Home",
+            heading: "Edit Article ",
+            slot1: "Rename Your Blog",
+            slot2: "Content",
+            button1: "Save Changes",
+            button2: "Back To Article's Home Page",
+            blog: row
         });
     });
+});
 
-    /* 
-        Publish a new article (submit)
-        INPUT: user clicks on the publish button in Author's Home Page
-        OUTPUT: status is updated in the database from "rough" to "published"
-   */
-    router.post('/:id', (req, res) => {
-        const id = req.params.id;
-        const status = "published";
-        const published_at = moment().format('YYYY-MM-DD HH:mm:ss');
-        const query = 'UPDATE blogs SET status = ?, published_at = ? WHERE id = ?';
-
-        db.run(query, [status, published_at, id], function(err)
-        {
-            if (err)
-            {
-                return console.log(err.message);
-            }
-            res.redirect('/author/blogs');
-        });
+/* 
+    Render edit-article page 
+    INPUT: user clicks on the edit button in Author's Home Page
+    OUTPUT: article with id in the req.params is updated in the database
+*/
+router.post('/edit/:id', (req, res) => {
+    const { title, content } = req.body;
+    const id = req.params.id;
+    const modified_at = moment().format('YYYY-MM-DD HH:mm:ss');
+    db.run("UPDATE blogs SET title = ?, content = ?, last_modified = ? WHERE id = ?", [title, content, modified_at, id], function (err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        res.redirect('/author/blogs');
     });
+});
 
-    /* 
-        Render edit-article page 
-        INPUT: user clicks on the publish button in Author's Home Page
-        OUTPUT: status is updated in the database from "rough" to "published"
-   */
-    router.get('/edit/:id', (req, res) => {
-        const id = req.params.id;
-        db.get("SELECT * FROM blogs WHERE id = ?", [id], (err, row) => {
-            if (err) 
-            {
+/* 
+    Delete Article
+    INPUT: user clicks on the delete button in Author's Home Page
+    OUTPUT: first comments for that article are delete, followed by likes, then dislikes, then views and finally the blog 
+*/
+router.post('/delete/:id', authorizeUser, (req, res) => {
+    const id = req.params.id;
+
+    // First deleting comments associated with the blog
+    const deleteCommentsQuery = 'DELETE FROM comments WHERE blog_id = ?';
+    db.run(deleteCommentsQuery, [id], function (err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        // Then deleting likes associated with the blog
+        const deleteLikesQuery = "DELETE FROM likes WHERE blog_id = ?";
+        db.run(deleteLikesQuery, [id], function (err) {
+            if (err) {
                 return console.log(err.message);
             }
-            res.render('edit-article', {  title: 'Edit Article' ,
-                                            name: "Pen Your Thoughts",
-                                            nav1: "Home",
-                                            heading: "Edit Article ",
-                                            slot1: "Rename Your Blog",
-                                            slot2: "Content",
-                                            button1: "Save Changes",
-                                            button2: "Back To Article's Home Page",
-                                            blog: row 
-                                        });
-        });
-    });
 
-    /* 
-        Render edit-article page 
-        INPUT: user clicks on the edit button in Author's Home Page
-        OUTPUT: article with id in the req.params is updated in the database
-   */
-    router.post('/edit/:id', (req, res) => {
-        const { title, content } = req.body;
-        const id = req.params.id;
-        const modified_at = moment().format('YYYY-MM-DD HH:mm:ss');
-        db.run("UPDATE blogs SET title = ?, content = ?, last_modified = ? WHERE id = ?", [title, content, modified_at, id], function(err)
-        {
-            if (err)
-            {
-                return console.log(err.message);
-            }
-            res.redirect('/author/blogs');
-        });
-    });
-
-    /* 
-        Delete Article
-        INPUT: user clicks on the delete button in Author's Home Page
-        OUTPUT: first comments for that article are delete, followed by likes, then dislikes, then views and finally the blog 
-   */
-    router.post('/delete/:id', (req, res) => {
-        const id = req.params.id;
-
-        // First deleting comments associated with the blog
-        const deleteCommentsQuery = 'DELETE FROM comments WHERE blog_id = ?';
-        db.run(deleteCommentsQuery, [id], function (err) 
-        {
-            if (err) 
-            {
-                return console.log(err.message);
-            }
-            // Then deleting likes associated with the blog
-            const deleteLikesQuery = "DELETE FROM likes WHERE blog_id = ?"; 
-            db.run(deleteLikesQuery, [id], function (err) 
-            {
-                if (err)
-                {
+            // Then deleting dislikes associated with the blog
+            const deleteDislikesQuery = "DELETE FROM dislikes WHERE blog_id = ?";
+            db.run(deleteDislikesQuery, [id], function (err) {
+                if (err) {
                     return console.log(err.message);
                 }
-
-                // Then deleting dislikes associated with the blog
-                const deleteDislikesQuery = "DELETE FROM dislikes WHERE blog_id = ?"; 
-                db.run(deleteDislikesQuery, [id], function (err) 
-                {
-                    if (err)
-                    {
+                // then deleting reactions to the blog
+                const deleteReactionsQuery = 'DELETE FROM reactions WHERE id = ?';
+                db.run(deleteReactionsQuery, [id], function (err) {
+                    if (err) {
                         return console.log(err.message);
                     }
-                    // then deleting reactions to the blog
-                    const deleteReactionsQuery = 'DELETE FROM reactions WHERE id = ?';
-                    db.run(deleteReactionsQuery, [id], function (err) 
-                    {
-                        if (err)
-                        {
+
+                    // then deteing views 
+                    const deleteViewsQuery = 'DELETE FROM views WHERE blog_id = ?';
+                    db.run(deleteViewsQuery, [id], function (err) {
+                        if (err) {
                             return console.log(err.message);
                         }
 
-                        // then deteing views 
-                        const deleteViewsQuery = 'DELETE FROM views WHERE blog_id = ?';
-                        db.run(deleteViewsQuery, [id], function (err) 
-                        {
-                            if (err)
-                            {
+                        // then finally deleting the blog
+                        const deleteBlogQuery = 'DELETE FROM blogs WHERE id = ?';
+                        db.run(deleteBlogQuery, [id], function (err) {
+                            if (err) {
                                 return console.log(err.message);
                             }
+                            else {
+                                res.redirect('/author/blogs')
+                            }
 
-                            // then finally deleting the blog
-                            const deleteBlogQuery = 'DELETE FROM blogs WHERE id = ?';
-                            db.run(deleteBlogQuery, [id], function (err) 
-                            {
-                                if (err)
-                                {
-                                    return console.log(err.message);
-                                }
-                                else {
-                                    res.redirect('/author/blogs')
-                                }
-                                
-                            }); 
-                        });  
+                        });
                     });
                 });
             });
         });
     });
+});
 
-    // Export the router object so index.js can access it
-    module.exports = router;
+// Export the router object so index.js can access it
+module.exports = router;
 
 /* END OF CODE SELF-WRITTEN */
